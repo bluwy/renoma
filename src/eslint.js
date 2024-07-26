a268f5c7ce3a837611fedaa04e75b31b72b6be1e
bluwy <bjornlu.dev@gmail.com>
2024-07-26 20:28:16 +0800

    Add more regex checks

--- src/eslint.js
+++ src/eslint.js
@@ -61,6 +61,31 @@ const baseEslintConfig = {
         regexp
       },
       rules: {
+        // === regexp possible errors ===
+        'regexp/no-contradiction-with-assertion': 'error',
+        // Subjective
+        // 'regexp/no-control-character': 'error',
+        'regexp/no-dupe-disjunctions': 'error',
+        'regexp/no-empty-alternative': 'error',
+        'regexp/no-empty-capturing-group': 'error',
+        'regexp/no-empty-character-class': 'error',
+        'regexp/no-empty-group': 'error',
+        'regexp/no-empty-lookarounds-assertion': 'error',
+        'regexp/no-escape-backspace': 'error',
+        'regexp/no-invalid-regexp': 'error',
+        'regexp/no-lazy-ends': 'error',
+        'regexp/no-misleading-capturing-group': 'error',
+        'regexp/no-misleading-unicode-character': 'error',
+        'regexp/no-missing-g-flag': 'error',
+        'regexp/no-optional-assertion': 'error',
+        'regexp/no-potentially-useless-backreference': 'error',
+        'regexp/no-super-linear-backtracking': 'error',
+        'regexp/no-super-linear-move': 'error',
+        'regexp/no-useless-assertions': 'error',
+        'regexp/no-useless-backreference': 'error',
+        'regexp/no-useless-dollar-replacements': 'error',
+        'regexp/strict': 'error',
+        // === regexp best practices ===
         'regexp/confusing-quantifier': 'error',
         'regexp/control-character-escape': 'error',
         'regexp/negation': 'error',
